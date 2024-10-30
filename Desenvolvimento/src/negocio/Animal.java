6268f919e07d9b6c154ebb48b6b0e3c143d50c6c
GiuliaVerse <giulia.casteluci@pucpr.edu.br>
2024-10-30 10:38:18 -0300

    feat : adiciona setters and getters ara a classe animal

--- Desenvolvimento/src/negocio/Animal.java
+++ Desenvolvimento/src/negocio/Animal.java
@@ -17,4 +17,52 @@ public abstract class Animal {
         this.raca = raca;
     }
 
+    // Getters
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public boolean isCastracao() {
+        return castracao;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setCastracao(boolean castracao) {
+        this.castracao = castracao;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
 }
