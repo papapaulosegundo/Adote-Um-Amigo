54b6f2c458696bbcbb1d623b9ed3a67655329ce5
papapaulosegundo <pcesar.muchalski@gmail.com>
2024-11-06 00:25:23 -0300

    SA-003-Login

--- Desenvolvimento/src/negocio/Abrigo.java
+++ Desenvolvimento/src/negocio/Abrigo.java
@@ -6,7 +6,36 @@ import java.util.List;
 import javax.swing.*;
 
 public class Abrigo {
+    private List<Usuario> usuarios = new ArrayList<Usuario>();
     private List<Animal> animais = new ArrayList<Animal>();
+    private Usuario usuarioAtivo;
+
+    public Abrigo() {
+        // usuario fixo para teste
+        usuarios.add(new Usuario("Paulo", "paulo", "123"));
+    }
+
+    public boolean buscaUsuario(String login, String senha) {
+        for( Usuario user : usuarios ) {
+            if( user.getLogin().equals(login) && 
+                user.getSenha().equals(senha)) {
+                    usuarioAtivo = user;
+                    return true;
+            }
+        }   
+        return false;
+    }
+    public boolean login() {
+        String login = JOptionPane.showInputDialog("Informe seu login");
+        String senha = JOptionPane.showInputDialog("Informe sua senha: ");
+        if( !buscaUsuario(login, senha)) {
+            JOptionPane.showMessageDialog(null, "Usuario não cadastrado", "Erro", JOptionPane.ERROR_MESSAGE);
+            return false;
+        } else {
+            JOptionPane.showMessageDialog(null, "Usuario logado com sucesso!");
+            return true;
+        }
+    }
 
     public void adotar() {
 
---
+++ Desenvolvimento/src/negocio/Usuario.java
@@ -0,0 +1,22 @@
+package negocio;
+
+public class Usuario {
+
+    private String nome;
+    private String login;
+    private String senha;
+
+    public Usuario(String nome, String login, String senha) {
+        this.nome = nome;
+        this.login = login;
+        this.senha = senha;
+    }   
+
+    public String getLogin() {
+        return login;
+    }
+    public String getSenha() {
+        return senha;
+    }
+
+}
